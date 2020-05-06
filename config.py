9d0eb34c6e3d742ddbed85f16bb1863b6c136d7a
Dean Pierce <pierce403@gmail.com>
2020-05-06 14:07:41 -0700

    shuffling some config stuff

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-ELASTICSEARCH_URL = ''
-SCOPE_DOC = os.path.join(basedir, 'config/scope.txt')
-BLACKLIST_DOC = os.path.join(basedir, 'config/blacklist.txt')
-RESULTS_PER_PAGE = 100
-PREVIEW_LENGTH = 100
