47f382637b5c0bd5e4d5ea29ef359f1cbe0022db
Sn0x0py <62267468+Sn0x0py@users.noreply.github.com>
2020-10-29 15:17:00 -0700

    Remove nweb-agent-docker

--- nweb-agent-docker/config.py
+++
@@ -1,8 +0,0 @@
-import os
-class Config:
-
-    def __init__(self):
-        self.server = 'https://nweb.io/'
-        self.max_threads = 16
-        self.timeout = 360
-        self.submit_token = os.environ.get("submit_token") or None
