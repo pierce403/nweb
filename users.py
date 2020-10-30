c8676bf9d17a5cc60673d8a60912d81fe467ca6c
Dean Pierce <pierce403@gmail.com>
2020-10-29 18:02:43 -0700

    tightening the thread pool even more

--- users.py
+++ users.py
@@ -15,8 +15,8 @@ def setup(app):
   print('!!! SETTING UP THE APP')
   try:
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_COBALT_URL']
-    app.config['SQLALCHEMY_POOL_SIZE'] = 3
-    app.config['SQLALCHEMY_MAX_OVERFLOW'] = 6
+    app.config['SQLALCHEMY_POOL_SIZE'] = 1
+    app.config['SQLALCHEMY_MAX_OVERFLOW'] = 4
     #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
   except Exception as e:
     print("[-] DBURI invalid "+str(e))
