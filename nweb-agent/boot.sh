7a5daec144c60993b653b5a6e169d0c5b33a9a29
Dean Pierce <pierce403@gmail.com>
2021-03-19 12:20:47 -0700

    seperating nweb-agent into its own repo

--- nweb-agent/boot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /opt/nweb/nweb-agent/
-python3 -i ./nweb_agent.py $submit_token
--- nweb-agent/config.py
+++
@@ -1,9 +0,0 @@
-import os
-class Config:
-
-    def __init__(self):
-        self.server = 'https://nweb.io/'
-        self.max_threads = 3
-        self.timeout = 360
-        self.submit_token = os.environ.get("SUBMITTOKEN") or None
-
