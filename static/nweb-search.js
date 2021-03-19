b832c6933e13945c5e5429feac77f53af224ceaf
Dean Pierce <pierce403@gmail.com>
2021-03-19 14:28:03 -0700

    wrong message box

--- static/nweb-search.js
+++ static/nweb-search.js
@@ -2,9 +2,9 @@ function nwebSearch()
 {
   console.log("HELLO THERE");
 
-  document.getElementById("msg").textContent = "Hey, thanks for searching";
+  document.getElementById("message").textContent = "Hey, thanks for searching";
 
-  fetch('https://nweb.io/?q=hostname:softbank219168179195.bbtec.net&f=json')
+  fetch('search?q=hostname:softbank219168179195.bbtec.net&f=json')
     .then(response => response.json())
     .then(data => console.log(data));
 }
